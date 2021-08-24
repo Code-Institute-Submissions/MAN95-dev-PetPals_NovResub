201ec1937fcf2d4a696bdc714027c2cc2d34cbad
MAN95-dev <michelleamynewell@gmail.com>
2021-08-24 12:53:05 +0000

    Customize admin

--- items/admin.py
+++ items/admin.py
@@ -2,5 +2,21 @@ from django.contrib import admin
 from .models import Item, Type
 
 # Register your models here.
-admin.site.register(Item)
-admin.site.register(Type)
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+class TypeAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Type, TypeAdmin)
