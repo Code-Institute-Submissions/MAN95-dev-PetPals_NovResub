d8d542fbf5c0367260941da03d4bf3b0c6857550
MAN95-dev <michelleamynewell@gmail.com>
2021-09-06 07:48:57 +0000

    Add onsale app

---
+++ onsale/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Onsale(models.Model):
+    type = models.ForeignKey('Type', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    instructions = models.TextField()
+    ingredients = models.TextField()
+    faq = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    onsale_price = models.DecimalField(max_digits=6, decimal_places=2)
+    savings = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
