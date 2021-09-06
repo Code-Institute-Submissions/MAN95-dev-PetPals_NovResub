da1b7b4863463628c316577b7d25786ef294d3a0
MAN95-dev <michelleamynewell@gmail.com>
2021-09-06 10:03:58 +0000

    Add more fields to the items model

--- items/models.py
+++ items/models.py
@@ -16,6 +16,10 @@ class Item(models.Model):
     type = models.ForeignKey('Type', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    instructions = models.TextField(null=True, blank=True)
+    ingredients = models.TextField(null=True, blank=True)
+    questions = models.TextField(null=True, blank=True)
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(null=True, blank=True)
 
