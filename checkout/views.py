8fed7864968901b93f8570f3084c81e1d5672e2e
MAN95-dev <michelleamynewell@gmail.com>
2021-09-13 15:10:56 +0000

    Complete webhook handler

--- checkout/views.py
+++ checkout/views.py
@@ -9,6 +9,7 @@ from items.models import Item
 from cart.contexts import cart_contents
 
 import stripe
+import json
 
 @require_POST
 def cache_checkout_data(request):
@@ -30,7 +31,7 @@ def cache_checkout_data(request):
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
-    
+
     if request.method == 'POST':
         cart = request.session.get('cart', {})
 
@@ -47,7 +48,11 @@ def checkout(request):
         }
         order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_cart = json.dumps(cart)
+            order.save()
             for item_id, item_data in cart.items():
                 try:
                     item = Item.objects.get(id=item_id)
@@ -80,9 +85,6 @@ def checkout(request):
         current_cart = cart_contents(request)
         total = current_cart['grand_total']
         stripe_total = round(total * 100)
-
-        print(stripe_secret_key)
-
         stripe.api_key = stripe_secret_key
         intent = stripe.PaymentIntent.create(
             amount=stripe_total,
@@ -124,4 +126,3 @@ def checkout_success(request, order_number):
     }
 
     return render(request, template, context)
-
