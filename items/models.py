54cb755cd8f16e200d6ded603055d47b85d77bb6
MAN95-dev <michelleamynewell@gmail.com>
2021-09-20 10:44:21 +0000

    Fix cart and navbar items
    Cart is always white whether theree are items in it or not and spaced out nav items

--- items/models.py
+++ items/models.py
@@ -20,7 +20,23 @@ class Item(models.Model):
     ingredients = models.TextField(null=True, blank=True)
     questions = models.TextField(null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    onsale = models.BooleanField(default=False, null=True, blank=True)
+    onsale_price = models.DecimalField(null=True, max_digits=6, decimal_places=2)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
+
+    def apply_discount(self):
+        """
+        Update grand total each time a line item is added,
+        accounting for delivery costs.
+        """
+        self.onsale_price = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
+        if self.onsale_price:
+            self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+        else:
+            self.price
+        self.grand_total = self.order_total + self.delivery_cost
+        self.save()
+
