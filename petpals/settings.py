a76dc8acd1073fceb83ae58c7875763df2124abc
MAN95-dev <michelleamynewell@gmail.com>
2021-09-24 06:02:50 +0000

    Add login link to checkout button if user in not authenticated

--- petpals/settings.py
+++ petpals/settings.py
@@ -209,7 +209,7 @@ DEFAULT_FROM_EMAIL = 'Sales@petpals.com'
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+    DEFAULT_FROM_EMAIL = 'petpals@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
