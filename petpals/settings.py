72c989a8121b40c69fdad5f65f5095be5155cfcf
MAN95-dev <michelleamynewell@gmail.com>
2021-09-26 01:57:02 +0000

    Fix bug in reviews
    Add reviews id to else  statement to stop reviiews showing in all tabs

--- petpals/settings.py
+++ petpals/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+#DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['petpals-milestone-4.herokuapp.com', 'localhost']
 
