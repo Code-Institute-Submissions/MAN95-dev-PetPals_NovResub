f809c85299139605ee2bcb5e79e7fe1053abf545
MAN95-dev <michelleamynewell@gmail.com>
2021-11-10 01:02:42 +0000

    Set debug to false

--- petpals/settings.py
+++ petpals/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['petpals-milestone-4.herokuapp.com', 'localhost']
 
